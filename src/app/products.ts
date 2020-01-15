a68467ed3ef9005ed0b841521e7acbfca4dbeafe
kennedysimiyumanyonge1 <Kennedy.SimiyuManyonge@mastercard.com>
2020-01-15 12:05:00 +0300

    adding a products component

---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens'
+    },
+    {
+        name: 'Phone Mine',
+        price: 699,
+        description: 'A phone with one of the best cameras'
+    },
+    {
+        name: 'Phone Standard',
+        price: 299,
+        description: ''
+    }
+];
